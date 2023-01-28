a0b30f470a42e632b0bca44ff8f9e4e63911d4e8
d3dx13 <zhidkov2846@gmail.com>
2023-01-28 17:09:57 +0300

    task G - Вкус таймлимита на 21 тесте )))) В принципе, справедливо, нужно немного более эфективно искать возможные маршруты на этом графе

--- src/G/G.cpp
+++ src/G/G.cpp
@@ -2,9 +2,68 @@
 // Created by zhidk on 28.01.2023.
 //
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+
+
 using namespace std;
 
 
 int main() {
+    int n = 2;
+    int x, y, k, from, to;
+    cin >> n;
+    vector <pair<int, int>> cities(n); // [x, y]
+    vector<int> cities_min_distance(n, INT32_MAX);
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        cin >> y;
+        cities[i] = make_pair(x, y);
+    }
+    cin >> k;
+    cin >> from;
+    cin >> to;
+    from--;
+    to--;
+
+    queue <pair<int, int>> cities_to_visit; // [city_id, city_prev_id]
+    cities_to_visit.push(make_pair(from, -1));
+    while (!cities_to_visit.empty()) {
+        pair<int, int> city_to_visit = cities_to_visit.front();
+        cities_to_visit.pop();
+
+        int city_id = city_to_visit.first;
+        int city_prev_id = city_to_visit.second;
+
+        if (city_prev_id < 0) {
+            cities_min_distance[city_id] = 0;
+        } else {
+            cities_min_distance[city_id] = min(cities_min_distance[city_id], cities_min_distance[city_prev_id] + 1);
+        }
+
+        for (int i = 0; i < size(cities_min_distance); i++) {
+            if (cities_min_distance[i] != INT32_MAX) {
+                continue;
+            }
+            unsigned long distance_to_unreached_city = 0;
+            distance_to_unreached_city += (unsigned long) abs(cities[i].first - cities[city_id].first);
+            distance_to_unreached_city += (unsigned long) abs(cities[i].second - cities[city_id].second);
+            if (distance_to_unreached_city <= k) {
+                cities_to_visit.push(make_pair(i, city_id));
+            }
+        }
+
+        // cout << city_id << " - " << city_prev_id << " - " << cities_min_distance[city_id] << "\n";
+        if (city_id == to) {
+            cout << cities_min_distance[to];
+            return 0;
+        }
+    }
+
+    cout << -1;
     return 0;
 }
--- src/G/cmake-build-debug/cmake_install.cmake
+++ src/G/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/WORKSPACE/Preparing-for-a-job-interview-at-Yandex/src/F
+# Install script for directory: C:/WORKSPACE/Preparing-for-a-job-interview-at-Yandex/src/G
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/WORKSPACE/Preparing-for-a-job-interview-at-Yandex/src/F/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/WORKSPACE/Preparing-for-a-job-interview-at-Yandex/src/G/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
