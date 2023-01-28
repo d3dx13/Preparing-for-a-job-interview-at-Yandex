06d5a8bb0e587234d1151736eb7c27b867bbac90
d3dx13 <zhidkov2846@gmail.com>
2023-01-28 16:50:36 +0300

    task A - Ищем сокровища из букавок

--- src/A/A.cpp
+++ src/A/A.cpp
@@ -2,3 +2,38 @@
 // Created by zhidk on 28.01.2023.
 //
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <ctime>
+#include <string>
+#include <set>
+
+
+using namespace std;
+
+
+int main() {
+    string J;
+    string S;
+    set<char> J_set;
+
+    cin >> J;
+    cin >> S;
+
+    for (int i = 0; i < J.length(); i++) {
+        J_set.insert(J[i]);
+    }
+
+    int S_in_J_counter = 0;
+    for (int i = 0; i < S.length(); i++) {
+        if (J_set.find(S[i]) != J_set.end()) {
+            S_in_J_counter += 1;
+        }
+    }
+
+    cout << S_in_J_counter;
+
+    return 0;
+}
