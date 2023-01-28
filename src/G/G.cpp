a2b56b493095711c1dfe39f6c77e3602635a9695
d3dx13 <zhidkov2846@gmail.com>
2023-01-28 20:58:02 +0300

    task D - честно, порог решения конченый, ведь N^2 не проходит, а N^2 * 2 (нет)... Забавно тут то, что можно решить задачу за N*LogN

--- src/G/G.cpp
+++ src/G/G.cpp
@@ -17,7 +17,7 @@ int main() {
     int n = 2;
     int x, y, k, from, to;
     cin >> n;
-    vector <pair<int, int>> cities(n); // [x, y]
+    vector<pair<int, int>> cities(n); // [x, y]
     vector<int> cities_min_distance(n, INT32_MAX);
     for (int i = 0; i < n; i++) {
         cin >> x;
@@ -30,8 +30,9 @@ int main() {
     from--;
     to--;
 
-    queue <pair<int, int>> cities_to_visit; // [city_id, city_prev_id]
+    queue<pair<int, int>> cities_to_visit; // [city_id, city_prev_id]
     cities_to_visit.push(make_pair(from, -1));
+    cities_min_distance[from] = 0;
     while (!cities_to_visit.empty()) {
         pair<int, int> city_to_visit = cities_to_visit.front();
         cities_to_visit.pop();
@@ -39,22 +40,20 @@ int main() {
         int city_id = city_to_visit.first;
         int city_prev_id = city_to_visit.second;
 
-        if (city_prev_id < 0) {
-            cities_min_distance[city_id] = 0;
-        } else {
-            cities_min_distance[city_id] = min(cities_min_distance[city_id], cities_min_distance[city_prev_id] + 1);
-        }
-
         for (int i = 0; i < size(cities_min_distance); i++) {
             if (cities_min_distance[i] != INT32_MAX) {
                 continue;
             }
-            unsigned long distance_to_unreached_city = 0;
-            distance_to_unreached_city += (unsigned long) abs(cities[i].first - cities[city_id].first);
-            distance_to_unreached_city += (unsigned long) abs(cities[i].second - cities[city_id].second);
+            unsigned int distance_to_unreached_city = 0;
+            distance_to_unreached_city += (unsigned int) abs(cities[i].first - cities[city_id].first);
+            distance_to_unreached_city += (unsigned int) abs(cities[i].second - cities[city_id].second);
+            // cout << "new try: " << city_id + 1 << " -> " << i + 1;
             if (distance_to_unreached_city <= k) {
                 cities_to_visit.push(make_pair(i, city_id));
+                cities_min_distance[i] = min(cities_min_distance[i], cities_min_distance[city_id] + 1);
+                // cout << " and add";
             }
+            // cout << "\n";
         }
 
         // cout << city_id << " - " << city_prev_id << " - " << cities_min_distance[city_id] << "\n";
