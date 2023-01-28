b3e89d6124f59fc8d88d2ce8841939c35f810496
d3dx13 <zhidkov2846@gmail.com>
2023-01-28 17:01:25 +0300

    ZERO - Когда настолько лень копипастить, что сделал Шаблон

---
+++ src/C/cmake-build-debug/CMakeFiles/simple_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_example.dir/C.cpp.obj"
+  "simple_example.pdb"
+  "simple_example.exe"
+  "simple_example.exe.manifest"
+  "libsimple_example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/ZERO/ZERO.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zhidk on 28.01.2023.
+//
+
+using namespace std;
+
+
+int main() {
+    return 0;
+}
